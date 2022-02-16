c5bf642b554784afc22f8d3883f3c564bcf8c683
lahincapie <alejandra612@hotmail.com>
2022-02-15 22:11:12 -0500

    proyect

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''python script'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    '''function to check nbre of sub'''
+    requestpost = requests.get("https://www.reddit.com/r/{}/hot.json".format(
+        subreddit), headers={"User-Agent": "My User Agent"}, params={"after": after})
+    if requestpost.status_code != 200:
+        return(None)
+    request_data = requestpost.json()
+    st = request_data["data"]["after"]
+    child = request_data["data"]["children"]
+    for c in child:
+        hot_list.append(c["data"]["title"])
+    if st is not None:
+        recurse(subreddit, hot_list, st)
+    return(hot_list)
